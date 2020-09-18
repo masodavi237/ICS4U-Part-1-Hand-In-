2bd1241f2ccba93d03655c19b754268fd2d04e6a
David Mason <MasoDavi237@GoTVDSB.ca>
2020-09-18 09:26:05 -0400

    Complete

--- favMovie/Program.cs
+++ favMovie/Program.cs
@@ -10,6 +10,34 @@ namespace favMovie
     {
         static void Main(string[] args)
         {
+
+
+            string firstName = "David";
+
+            string favMovie = "The Matrix";
+
+            Console.WriteLine($"Good morning {firstName.ToLower()}, I just want to let you know that your favourite movie, {favMovie.ToLower()}, sucks! Have a good day...");
+
+            favMovie = favMovie.ToUpper();
+
+            Console.WriteLine(favMovie);
+
+            Console.WriteLine(favMovie.Contains("THE"));
+
+            favMovie = favMovie.Replace('A', '@');
+            favMovie = favMovie.Replace('E', '3');
+
+            Console.WriteLine(favMovie);
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 }
